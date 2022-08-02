925cfcc284b43e0adfff87fc82d81fe6ce79cf5f
Amanda Ross <amikaross@gmail.com>
2022-08-02 15:35:32 -0600

    Reassign string variables and add puts

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
 owner_name = "Amanda"
 pet_name = "Haku"
-species = "Dog"
+species = "dog"
 owner_age = 32
 dog_age = 3
 years_owned = 2
@@ -10,3 +10,10 @@ weekly_barks = 56.9
 neutered = true
 vaccinated = true
 aggressive = true
+owner_name = "Lee"
+pet_name = "Corolla"
+species = "beater"
+
+puts "The pet is named " + pet_name + " and they are owned by " + owner_name + "."
+puts "This pet is a " + species + "."
+puts pet_name + " only needs #{daily_water} liters of water daily."
